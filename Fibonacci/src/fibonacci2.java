e4f65e2f83d0d060d19a5f92e0a9ad30a29ed0bd
Admin <Admin@Nicola-PC.fritz.box>
2015-10-12 21:20:13 +0200

    Hausaufgabe 13.10.15

---
+++ Fibonacci/src/fibonacci2.java
@@ -0,0 +1,20 @@
+
+public class fibonacci2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+long zahl1 = 1,zahl2 = 1,i=0;
+while(i<=50){
+	if(i%2==0){
+		System.out.println(zahl1);
+		zahl1+=zahl2;
+	}
+	else{
+		System.out.println(zahl2);
+		zahl2+=zahl1;
+	}
+	
+	i++;
+	}
+}
+}
